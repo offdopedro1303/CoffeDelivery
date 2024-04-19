77dec1a47f188995673487f54f3362019612a31a
pedrohqa <pedrohenriqueqaraujo@gmail.com>
2024-04-19 15:25:43 -0300

    pedro

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,22 +1,43 @@
 import {
     FooterContainer,
-    NavContainer,
+    Parte1,
     LogoContainer,
+    Termo,
+    Social,
+    So1,
+    So2,
+    So3,
     
   } from "./style";
 
   import Logo from "../../assets/Logo.svg";
- 
+  import { FaFacebook } from "react-icons/fa";
+  import { FaInstagram} from "react-icons/fa";
+  import { FaWhatsapp } from "react-icons/fa";
   
   export function Footer() {
     return (
-      <FooterContainer>
-            <NavContainer>
-                    <LogoContainer>
-                    <img src={Logo} alt="Logo do Café Delivery" />  
-                    </LogoContainer> 
-            </NavContainer>
-      </FooterContainer>
+        <FooterContainer>
+            <Parte1>
+                <LogoContainer>
+                 <img src={Logo} alt="Logo do Café Delivery" />         
+                </LogoContainer> 
+                <Termo>
+                    <p>Termos de uso</p>  
+                </Termo>
+                <Social>
+                    <So1>
+                        <FaWhatsapp/>
+                    </So1>
+                    <So2>
+                        <FaInstagram/>
+                    </So2>
+                    <So3>                        
+                        <FaFacebook/>
+                    </So3>
+                </Social>
+            </Parte1>
+        </FooterContainer>
     );
   }
 
--- src/components/Footer/style.ts
+++ src/components/Footer/style.ts
@@ -2,29 +2,47 @@ import styled from "styled-components";
 
 export const FooterContainer = styled.div`
   width: 100%;
-  padding: 60px 160px;
-  background-color: #EBE5F9;
+  padding: 60px;
+  background-color: #EBE5F9;  
 `;
 
-export const LogoContainer = styled.div`
+export const Parte1 = styled.div`
   display: flex;
-  align-items: center;
-`;
-
-export const NavContainer = styled.nav`
-  display: flex;
-  flex-direction: row;
-  align-items: center;
+  align-items: center; 
   justify-content: space-between;
 `;
 
-export const LocationContainer = styled.div`
-  display: flex;
+export const LogoContainer = styled.div`
   align-items: center;
+  position: relative;
+`;
+export const Termo = styled.div`
+color: #9263F5 ;
+`;  
+
+export const Social = styled.div`
+    cursor: pointer;
+    font-size: 1.7em;
+    color:#9263F5;
+    align-items: center;
+    position: relative;
+    display: flex;
 `;
 
-export const Footer = styled.div`
-  display: flex;
-  align-items: center;
+export const So1 = styled.div`
+
+ margin-left: 0px;
+ margin-right: 0px;
 `;
 
+export const So2 = styled.div`
+
+ margin-left: 25px;
+ margin-right: 25px;
+`;
+
+export const So3 = styled.div`
+
+ margin-left: 0px;
+ margin-right: 0px;
+`;
