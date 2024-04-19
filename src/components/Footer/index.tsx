d8457c5fe94d438c507141a919c22d02d6fbb289
pedrohqa <pedrohenriqueqaraujo@gmail.com>
2024-04-19 10:02:05 -0300

    pedroa

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,45 +1,22 @@
 import {
-    HeaderContainer,
+    FooterContainer,
     NavContainer,
-    LocationContainer,
     LogoContainer,
-    ShopIcon,
-    LocationIcon,
   } from "./style";
   import Logo from "../../assets/Logo.svg";
-  import { MapPinLine, ShoppingCart } from "phosphor-react";
-  import { useCart } from "../../context/CartContext";
   import { NavLink } from "react-router-dom";
   
   export function Footer() {
-    const { coffesCount } = useCart();
-  
     return (
-      <HeaderContainer>
+      <FooterContainer>
         <NavContainer>
-          <NavLink to={"/"} title="Página Inicial">
-            <LogoContainer>
-              <img src={Logo} alt="Logo do Café Delivery" />
-            </LogoContainer>
-          </NavLink>
-          <LocationContainer>
-            <LocationIcon>
-              <MapPinLine size={22} alt="ponto de localização" />
-              <p>Santo André, SP</p>
-            </LocationIcon>
-            <NavLink to="/Shop" title="Pagina para Finalizar a compra !">
-              <ShopIcon>
-                <ShoppingCart
-                  size={22}
-                  color={"#C47F17"}
-                  alt="Carrinho de compra"
-                />
-                <div>{coffesCount}</div>
-              </ShopIcon>
+            <NavLink to={"/"} title="Página Inicial">
+                <LogoContainer>
+                <img src={Logo} alt="Logo do Café Delivery" />
+                </LogoContainer>            
             </NavLink>
-          </LocationContainer>
         </NavContainer>
-      </HeaderContainer>
+      </FooterContainer>
     );
   }
   
--- src/components/Footer/style.ts
+++ src/components/Footer/style.ts
@@ -1,9 +1,9 @@
 import styled from "styled-components";
 
-export const HeaderContainer = styled.div`
+export const FooterContainer = styled.div`
   width: 100%;
-  padding: 36px 160px;
-  background-color: ${(props) => props.theme["background"]};
+  padding: 60px 160px;
+  background-color: #EEDCD1;
 `;
 
 export const LogoContainer = styled.div`
@@ -22,46 +22,5 @@ export const LocationContainer = styled.div`
   display: flex;
   align-items: center;
 `;
-export const LocationIcon = styled.div`
-  display: flex;
-  gap: 4px;
-  padding: 8px;
-  font-size: 14px;
-  font-family: "Roboto";
-  line-height: "130%";
-  font-weight: 600;
-  border-radius: 10px;
-  align-items: center;
-  color: var(--Brand-Purple-Dark, #4b2995);
-  background-color: ${(props) => props.theme["purple-light"]};
-`;
 
-export const ShopIcon = styled.div`
-  display: flex;
-  padding: 8px;
-  justify-content: center;
-  text-decoration: none;
-  align-items: center;
-  gap: 4px;
-  border-radius: 8px;
-  margin-left: 1rem;
-  background-color: ${(props) => props.theme["yellow-light"]};
-  :active {
-    color: ${(props) => props.theme["yellow-light"]};
-    background-color: ${(props) => props.theme["yellow-light"]};
-  }
-`;
 
-export const testLink = styled.div`
-  ${(props) => props.theme["background"]}
-`;
-
-export const Shop = styled.img`
-  height: 22px;
-  width: 22px;
-`;
-
-export const Pin = styled.img`
-  height: 22px;
-  width: 22px;
-`;
